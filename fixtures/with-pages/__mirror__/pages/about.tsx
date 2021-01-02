f6f4f80a8e577e6cb1787d7b43c594bd6ef75248
remorses <beats.by.morse@gmail.com>
2021-01-02 15:59:30 +0100

    snaps

---
+++ fixtures/with-pages/__mirror__/pages/about.tsx
@@ -0,0 +1,7 @@
+import react_cjsImport0 from "/web_modules/react/index.js?namespace=file";
+const React = react_cjsImport0 && react_cjsImport0.__esModule ? react_cjsImport0.default : react_cjsImport0;;
+export default function Page() {
+  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("p", null, "About me:"), /* @__PURE__ */ React.createElement("p", null, "I Am Me"), /* @__PURE__ */ React.createElement("p", null, "..."), /* @__PURE__ */ React.createElement("p", null, "cool"));
+}
+
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiL1VzZXJzL21vcnNlL0RvY3VtZW50cy9HaXRIdWIvZXNwYWNrL2ZpeHR1cmVzL3dpdGgtcGFnZXMvcGFnZXMvYWJvdXQudHN4Il0sCiAgInNvdXJjZXNDb250ZW50IjogWyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnXG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIFBhZ2UoKSB7XG4gICAgcmV0dXJuIChcbiAgICAgICAgPGRpdj5cbiAgICAgICAgICAgIDxwPkFib3V0IG1lOjwvcD5cbiAgICAgICAgICAgIDxwPkkgQW0gTWU8L3A+XG4gICAgICAgICAgICA8cD4uLi48L3A+XG4gICAgICAgICAgICA8cD5jb29sPC9wPlxuICAgICAgICA8L2Rpdj5cbiAgICApXG59XG4iXSwKICAibWFwcGluZ3MiOiAiQUFBQTtBQUVBO0FBQ0ksU0FDSSxvQ0FBQyxPQUFELE1BQ0ksb0NBQUMsS0FBRCxNQUFHLGNBQ0gsb0NBQUMsS0FBRCxNQUFHLFlBQ0gsb0NBQUMsS0FBRCxNQUFHLFFBQ0gsb0NBQUMsS0FBRCxNQUFHO0FBQUE7IiwKICAibmFtZXMiOiBbXQp9Cg==
--- fixtures/with-pages/__mirror__/pages/index.tsx
+++ fixtures/with-pages/__mirror__/pages/index.tsx
@@ -1,7 +1,12 @@
 import react_cjsImport0 from "/web_modules/react/index.js?namespace=file";
 const React = react_cjsImport0 && react_cjsImport0.__esModule ? react_cjsImport0.default : react_cjsImport0;;
-export default function App() {
-  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("p", null, "Ciao"));
+import {Link} from "/web_modules/react-router-dom/esm/react-router-dom.js?namespace=file";
+export default function Page() {
+  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("p", null, "Ciao"), /* @__PURE__ */ React.createElement("a", {
+    href: "/about"
+  }, "/about with a"), /* @__PURE__ */ React.createElement("br", null), /* @__PURE__ */ React.createElement(Link, {
+    to: "/about"
+  }, "/about with Link"));
 }
 
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiL1VzZXJzL21vcnNlL0RvY3VtZW50cy9HaXRIdWIvZXNwYWNrL2ZpeHR1cmVzL3dpdGgtcGFnZXMvcGFnZXMvaW5kZXgudHN4Il0sCiAgInNvdXJjZXNDb250ZW50IjogWyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnXG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIEFwcCgpIHtcbiAgICByZXR1cm4gKFxuICAgICAgICA8ZGl2PlxuICAgICAgICAgICAgPHA+Q2lhbzwvcD5cbiAgICAgICAgPC9kaXY+XG4gICAgKVxufVxuIl0sCiAgIm1hcHBpbmdzIjogIkFBQUE7QUFFQTtBQUNJLFNBQ0ksb0NBQUMsT0FBRCxNQUNJLG9DQUFDLEtBQUQsTUFBRztBQUFBOyIsCiAgIm5hbWVzIjogW10KfQo=
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiL1VzZXJzL21vcnNlL0RvY3VtZW50cy9HaXRIdWIvZXNwYWNrL2ZpeHR1cmVzL3dpdGgtcGFnZXMvcGFnZXMvaW5kZXgudHN4Il0sCiAgInNvdXJjZXNDb250ZW50IjogWyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnXG5pbXBvcnQgeyBMaW5rIH0gZnJvbSAncmVhY3Qtcm91dGVyLWRvbSdcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gUGFnZSgpIHtcbiAgICByZXR1cm4gKFxuICAgICAgICA8ZGl2PlxuICAgICAgICAgICAgPHA+Q2lhbzwvcD5cbiAgICAgICAgICAgIDxhIGhyZWY9Jy9hYm91dCc+L2Fib3V0IHdpdGggYTwvYT5cbiAgICAgICAgICAgIDxici8+XG4gICAgICAgICAgICA8TGluayB0bz0nL2Fib3V0Jz4vYWJvdXQgd2l0aCBMaW5rPC9MaW5rPlxuICAgICAgICA8L2Rpdj5cbiAgICApXG59XG4iXSwKICAibWFwcGluZ3MiOiAiQUFBQTtBQUNBO0FBRUE7QUFDSSxTQUNJLG9DQUFDLE9BQUQsTUFDSSxvQ0FBQyxLQUFELE1BQUcsU0FDSCxvQ0FBQyxLQUFEO0FBQUEsSUFBRyxNQUFLO0FBQUEsS0FBUyxrQkFDakIsb0NBQUMsTUFBRCxPQUNBLG9DQUFDLE1BQUQ7QUFBQSxJQUFNLElBQUc7QUFBQSxLQUFTO0FBQUE7IiwKICAibmFtZXMiOiBbXQp9Cg==
