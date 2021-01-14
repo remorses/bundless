b41ac292c75ea6eeb9c5f72fcdb4473bb064712b
remorses <beats.by.morse@gmail.com>
2021-01-14 22:28:12 +0100

    fix openInEditorMiddleware

--- bundless/src/middleware/open-in-editor.ts
+++ bundless/src/middleware/open-in-editor.ts
@@ -1,8 +1,9 @@
 import { logger } from '..'
 import launchEditor from 'launch-editor'
 import { importPathToFile } from '../utils'
+import { Middleware } from 'koa'
 
-export function openInEditorMiddleware({ root }) {
+export function openInEditorMiddleware({ root }): Middleware {
     return function(ctx, next) {
         if (ctx.path !== '/__open-in-editor') {
             return next()
@@ -10,9 +11,7 @@ export function openInEditorMiddleware({ root }) {
         const { file = '' } = ctx.query || {}
         if (!file) {
             ctx.res.statusCode = 500
-            ctx.res.end(
-                `launch-editor-middleware: required query param "file" is missing.`,
-            )
+            ctx.body = `launch-editor-middleware: required query param "file" is missing.`
             return
         }
         const realPath = file.startsWith('/')
@@ -21,5 +20,7 @@ export function openInEditorMiddleware({ root }) {
 
         logger.debug(`Opening editor for ${file} at ${realPath}`)
         launchEditor(realPath)
+        ctx.res.statusCode = 200
+        ctx.body = `Opened ${realPath}`
     }
 }
