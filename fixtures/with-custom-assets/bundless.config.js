6d33cb3acbfdcf95147ed52c2357fff563a54d1d
remorses <beats.by.morse@gmail.com>
2021-02-19 14:47:13 +0100

    test uppercase extensions

--- fixtures/with-custom-assets/bundless.config.js
+++ fixtures/with-custom-assets/bundless.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    importableAssetsExtensions: ['.dat'],
+    importableAssetsExtensions: ['.dat', '.DAC'],
 }
--- fixtures/with-custom-assets/file.js
+++ fixtures/with-custom-assets/file.js
@@ -1,5 +1,5 @@
 import path from './file.dat'
 
+import('./x.DAC').then(console.log)
 
 export const text = 'This has been made in 2020, what a shitty year ' + path
-
